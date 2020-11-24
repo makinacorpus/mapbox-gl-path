d2f15cb634f7f329704fcffd9038457738230f3a
bastyen <bastien.alvez@makina-corpus.com>
2020-11-24 15:47:53 +0100

    add line

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export default class MapboxPathControl implements IControl {
   private map: Map | undefined;
   private pathControl: HTMLElement | undefined;
   private referencePoints: Feature[] = [];
+  private linesBetweenReferencePoints: Feature[] = [];
 
   constructor() {}
 
@@ -36,28 +37,44 @@ export default class MapboxPathControl implements IControl {
   private configureMap(): void {
     console.log("configureMap");
 
-    const referencePointsSource: GeoJSONSourceRaw = {
+    const pointsAndLinesSource: GeoJSONSourceRaw = {
       type: "geojson",
       data: { type: "FeatureCollection", features: [] },
     };
     const referencePointsLayerCircle: Layer = {
       id: "reference-points-circle",
       type: "circle",
-      source: "reference-points",
+      source: "points-and-lines",
       paint: { "circle-radius": 10, "circle-color": "#000000" },
+      filter: ["in", "$type", "Point"],
     };
     const referencePointsLayerText: Layer = {
       id: "reference-points-text",
       type: "symbol",
-      source: "reference-points",
+      source: "points-and-lines",
       paint: { "text-color": "#ffffff" },
-      layout: { "text-field": ["get", "id"] },
+      layout: { "text-field": ["get", "id"], "text-allow-overlap": true },
+      filter: ["in", "$type", "Point"],
     };
-    this.map?.addSource("reference-points", referencePointsSource);
-    this.map?.addLayer(referencePointsLayerCircle);
-    this.map?.addLayer(referencePointsLayerText);
+    const referencePointsLayerLine: Layer = {
+      id: "between-points-line",
+      type: "line",
+      source: "points-and-lines",
+      paint: { "line-color": "#000000" },
+      filter: ["in", "$type", "LineString"],
+    };
+    this.map!.addSource("points-and-lines", pointsAndLinesSource);
+    this.map!.addLayer(referencePointsLayerCircle);
+    this.map!.addLayer(referencePointsLayerText);
+    this.map!.addLayer(referencePointsLayerLine, "reference-points-circle");
 
-    this.map?.on("click", this.onClickMap.bind(this));
+    this.map!.on("click", this.onClickMap.bind(this));
+    this.map!.on("mouseenter", "reference-points-circle", () => {
+      this.map!.getCanvas().style.cursor = "pointer";
+    });
+    this.map!.on("mouseleave", "reference-points-circle", () => {
+      this.map!.getCanvas().style.cursor = "grab";
+    });
   }
 
   private onClickMap(event: MapMouseEvent): void {
@@ -73,10 +90,28 @@ export default class MapboxPathControl implements IControl {
     };
     this.referencePoints.push(referencePoint);
 
+    if (this.referencePoints.length > 1) {
+      const previousReferencePoint = this.referencePoints[
+        this.referencePoints.length - 2
+      ];
+      const lineBetweenReferencePoint: Feature = {
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          coordinates: [
+            (previousReferencePoint.geometry as any).coordinates,
+            [event.lngLat.lng, event.lngLat.lat],
+          ],
+        },
+        properties: { id: this.referencePoints.length + 1 },
+      };
+      this.linesBetweenReferencePoints.push(lineBetweenReferencePoint);
+    }
+
     const data: GeoJSON.FeatureCollection<GeoJSON.Geometry> = {
       type: "FeatureCollection",
-      features: this.referencePoints,
+      features: [...this.referencePoints, ...this.linesBetweenReferencePoints],
     };
-    (this.map?.getSource("reference-points") as GeoJSONSource).setData(data);
+    (this.map!.getSource("points-and-lines") as GeoJSONSource).setData(data);
   }
 }
