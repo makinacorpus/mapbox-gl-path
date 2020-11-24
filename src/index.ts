7323d2a87d6485f474cb6c1551f1bcf6186a7ad3
bastyen <bastien.alvez@makina-corpus.com>
2020-11-24 15:47:53 +0100

    move point

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {
   GeoJSONSource,
   GeoJSONSourceRaw,
   Layer,
+  MapboxGeoJSONFeature,
 } from "mapbox-gl";
 import "./mapbox-gl-path.css";
 
@@ -13,7 +14,9 @@ export default class MapboxPathControl implements IControl {
   private map: Map | undefined;
   private pathControl: HTMLElement | undefined;
   private referencePoints: Feature[] = [];
+  private selectedReferencePoint: Feature | undefined;
   private linesBetweenReferencePoints: Feature[] = [];
+  private onMovePointBind = this.onMovePoint.bind(this);
 
   constructor() {}
 
@@ -35,8 +38,6 @@ export default class MapboxPathControl implements IControl {
   }
 
   private configureMap(): void {
-    console.log("configureMap");
-
     const pointsAndLinesSource: GeoJSONSourceRaw = {
       type: "geojson",
       data: { type: "FeatureCollection", features: [] },
@@ -53,7 +54,10 @@ export default class MapboxPathControl implements IControl {
       type: "symbol",
       source: "points-and-lines",
       paint: { "text-color": "#ffffff" },
-      layout: { "text-field": ["get", "id"], "text-allow-overlap": true },
+      layout: {
+        "text-field": ["to-string", ["+", ["get", "index"], 1]],
+        "text-allow-overlap": true,
+      },
       filter: ["in", "$type", "Point"],
     };
     const referencePointsLayerLine: Layer = {
@@ -73,13 +77,27 @@ export default class MapboxPathControl implements IControl {
       this.map!.getCanvas().style.cursor = "pointer";
     });
     this.map!.on("mouseleave", "reference-points-circle", () => {
-      this.map!.getCanvas().style.cursor = "grab";
+      this.map!.getCanvas().style.cursor = "";
+    });
+    this.map!.on("mousedown", "reference-points-circle", (event) => {
+      event.preventDefault();
+      const referencePointsUnderMouse: MapboxGeoJSONFeature[] = this.map!.queryRenderedFeatures(
+        event.point,
+        {
+          layers: ["reference-points-circle"],
+        }
+      );
+
+      if (referencePointsUnderMouse && referencePointsUnderMouse.length > 0) {
+        this.map!.getCanvas().style.cursor = "grab";
+        this.selectedReferencePoint = referencePointsUnderMouse[0];
+        this.map!.on("mousemove", this.onMovePointBind);
+        this.map!.once("mouseup", this.onUpPoint.bind(this));
+      }
     });
   }
 
   private onClickMap(event: MapMouseEvent): void {
-    console.log("onClickMap");
-
     const referencePointIsUnderMouse: boolean = Boolean(
       this.map!.queryRenderedFeatures(event.point, {
         layers: ["reference-points-circle"],
@@ -93,7 +111,7 @@ export default class MapboxPathControl implements IControl {
           type: "Point",
           coordinates: [event.lngLat.lng, event.lngLat.lat],
         },
-        properties: { id: this.referencePoints.length + 1 },
+        properties: { index: this.referencePoints.length },
       };
       this.referencePoints.push(referencePoint);
 
@@ -110,7 +128,7 @@ export default class MapboxPathControl implements IControl {
               [event.lngLat.lng, event.lngLat.lat],
             ],
           },
-          properties: { id: this.referencePoints.length + 1 },
+          properties: { index: this.referencePoints.length },
         };
         this.linesBetweenReferencePoints.push(lineBetweenReferencePoint);
       }
@@ -124,5 +142,65 @@ export default class MapboxPathControl implements IControl {
       };
       (this.map!.getSource("points-and-lines") as GeoJSONSource).setData(data);
     }
+  }
+
+  private onMovePoint(event: MapMouseEvent): void {
+    this.map!.getCanvas().style.cursor = "grabbing";
+    (this.referencePoints[this.selectedReferencePoint!.properties!.index]
+      .geometry as any).coordinates = [event.lngLat.lng, event.lngLat.lat];
+
+    if (
+      this.selectedReferencePoint!.properties!.index === 0 &&
+      this.referencePoints.length > 1
+    ) {
+      (this.linesBetweenReferencePoints[
+        this.selectedReferencePoint!.properties!.index
+      ].geometry as any).coordinates = [
+        [event.lngLat.lng, event.lngLat.lat],
+        (this.linesBetweenReferencePoints[
+          this.selectedReferencePoint!.properties!.index
+        ].geometry as any).coordinates[1],
+      ];
+    } else if (
+      this.selectedReferencePoint!.properties!.index ===
+      this.referencePoints.length - 1
+    ) {
+      (this.linesBetweenReferencePoints[
+        this.selectedReferencePoint!.properties!.index - 1
+      ].geometry as any).coordinates = [
+        (this.linesBetweenReferencePoints[
+          this.selectedReferencePoint!.properties!.index - 1
+        ].geometry as any).coordinates[0],
+        [event.lngLat.lng, event.lngLat.lat],
+      ];
+    } else {
+      (this.linesBetweenReferencePoints[
+        this.selectedReferencePoint!.properties!.index
+      ].geometry as any).coordinates = [
+        [event.lngLat.lng, event.lngLat.lat],
+        (this.linesBetweenReferencePoints[
+          this.selectedReferencePoint!.properties!.index
+        ].geometry as any).coordinates[1],
+      ];
+      (this.linesBetweenReferencePoints[
+        this.selectedReferencePoint!.properties!.index - 1
+      ].geometry as any).coordinates = [
+        (this.linesBetweenReferencePoints[
+          this.selectedReferencePoint!.properties!.index - 1
+        ].geometry as any).coordinates[0],
+        [event.lngLat.lng, event.lngLat.lat],
+      ];
+    }
+
+    const data: GeoJSON.FeatureCollection<GeoJSON.Geometry> = {
+      type: "FeatureCollection",
+      features: [...this.referencePoints, ...this.linesBetweenReferencePoints],
+    };
+    (this.map!.getSource("points-and-lines") as GeoJSONSource).setData(data);
+  }
+
+  private onUpPoint(): void {
+    this.map!.off("mousemove", this.onMovePointBind);
+    this.map!.getCanvas().style.cursor = "grab";
   }
 }
