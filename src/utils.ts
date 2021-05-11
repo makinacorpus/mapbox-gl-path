bbc71e9813e9570dba70c521187754148470d28a
Florian Sommariva <florian.sommariva@makina-corpus.com>
2021-05-11 12:42:12 +0200

    Add `themeSelectionType` parameter to define directionTheme select UI

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { LngLat } from "mapbox-gl";
+import { DirectionsTheme, ThemeSelectionType } from "./index";
 
 export function createElement(tagName: string, props: object = {}) {
   const element = document.createElement(tagName);
@@ -6,4 +6,67 @@ export function createElement(tagName: string, props: object = {}) {
     element.setAttribute("type", "button");
   }
   return Object.assign(element, props);
+}
+
+export function selectThemesElement({
+  props,
+  themes,
+  themeSelectionType,
+}: {
+  props: object;
+  themes: DirectionsTheme[];
+  themeSelectionType: ThemeSelectionType;
+}) {
+  const pathControlSelection = createElement("div", {
+    className: `mapbox-gl-path-theme-selection mapbox-gl-path-theme-selection--${themeSelectionType}`,
+  });
+
+  // Find the selected index or pick the first one
+  let selectedIndex = themes.findIndex(({ selected }) => selected);
+  if (selectedIndex === -1) {
+    selectedIndex = 0;
+  }
+
+  // Build a select tag
+  if (themeSelectionType === "select") {
+    const select = createElement("select", props);
+    themes.forEach((theme, index) => {
+      const pathControlSelectOption = createElement("option", {
+        name: "mapbox-gl-path-theme-selection",
+        selected: selectedIndex === index,
+        textContent: theme.name,
+        value: theme.id.toString(),
+      });
+      select.append(pathControlSelectOption);
+    });
+    pathControlSelection.append(select);
+  }
+
+  // Build a radio list tag
+  if (themeSelectionType === "radioList") {
+    themes.forEach((theme, index) => {
+      const id = `${theme.name.split(" ").join("-")}-${theme.id.toString()}`;
+      const pathControlRadioLabel = createElement("div", {
+        className: "mapbox-gl-path-theme-selection__item",
+      });
+      const pathControlRadio = createElement("input", {
+        className: "mapbox-gl-path-theme-selection__radio",
+        checked: selectedIndex === index,
+        id,
+        name: "mapbox-gl-path-theme-selection",
+        type: "radio",
+        value: theme.id.toString(),
+        ...props,
+      });
+      const pathControlLabel = createElement("label", {
+        className: "mapbox-gl-path-theme-selection__radio-label",
+        htmlFor: id,
+        textContent: theme.name,
+      });
+      pathControlRadioLabel.append(pathControlRadio, pathControlLabel);
+      pathControlSelection.append(pathControlRadioLabel);
+    });
+  }
+
+  return pathControlSelection;
 }
