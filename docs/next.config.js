ccb3aad2d23eb79a0ed50d87ed6db512ac129fe8
Florian Sommariva <florian.sommariva@makina-corpus.com>
2021-10-13 11:25:37 +0200

    Add target: "serverless" to next.config.js

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,17 +2,20 @@ const withTM = require("next-transpile-modules")([
   "@makina-corpus/mapbox-gl-path",
 ]);
 
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
   unstable_staticImage: true,
-})
+});
 
-module.exports = withTM(withNextra({
-  webpack: (config) => {
-    Object.assign(config.resolve.alias, {
-      "mapbox-gl": "maplibre-gl",
-    });
-    return config;
-  },
-}));
+module.exports = withTM(
+  withNextra({
+    target: "serverless",
+    webpack: (config) => {
+      Object.assign(config.resolve.alias, {
+        "mapbox-gl": "maplibre-gl",
+      });
+      return config;
+    },
+  })
+);
